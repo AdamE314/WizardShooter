acfbf3d5e54458919236ecc1bc6b95002e4fa25d
Adam Eckert <clyde480@gmail.com>
2018-11-18 14:06:46 -0700

    Revert "Page 6 and 7"
    This reverts commit 1e7a4769933f9cdea7dfd5d2d8a3312c8f2ae4fc.

--- Assets/Scripts/bossSpawner.cs
+++ Assets/Scripts/bossSpawner.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System;
 using UnityEngine;
 
 public class bossSpawner : MonoBehaviour {
@@ -16,16 +15,14 @@ public class bossSpawner : MonoBehaviour {
     float spawnTimer;
 
     //Enemies
+    enum enm {bat, charger};
     public GameObject bat;
     public GameObject charger;
     public float arenaRadius;
     public Vector3 arenaCenter;
-    public int[,] enmSpawn;
-    public int myWave = 0;
 
 	// Use this for initialization
 	void Start () {
-        enmSpawn = defineSpawns(0);
         myAI = GetComponent<bossAI>();
         enmNum = 1;
         spawnTimer = spawnDelay;
@@ -41,15 +38,13 @@ public class bossSpawner : MonoBehaviour {
             spawnTimer -= Time.deltaTime;
             if (spawnTimer <= 0 && enmCount < enmNum)
             {
-                int waveEnmCount = 0;
-                //for(int i=0;i<)
                 //Spawning
                 GameObject _enm;
-                var _rand = UnityEngine.Random.value;
+                var _rand = Random.value;
                 if (_rand >= 0.5) _enm = bat; else _enm = charger;
 
-                Vector3 _temp = new Vector3(UnityEngine.Random.value-0.5f,0f, UnityEngine.Random.value - 0.5f).normalized;
-                Vector3 spawnOffset = _temp * (UnityEngine.Random.value * arenaRadius);
+                Vector3 _temp = new Vector3(Random.value-0.5f,0f, Random.value - 0.5f).normalized;
+                Vector3 spawnOffset = _temp * (Random.value * arenaRadius);
 
                 Vector3 enmSpawn = arenaCenter + spawnOffset;
 
@@ -70,31 +65,4 @@ public class bossSpawner : MonoBehaviour {
         }
 
 	}
-
-    int[,] defineSpawns(int bossid)
-    {
-        int[,] mySpawns = new int[GetComponent<bossAI>().myHealth, 2];
-
-        //Round 1 
-        mySpawns[0, 0] = 5; //Bats
-        mySpawns[0, 1] = 0; //Charger
-
-        //Round 2
-        mySpawns[1, 0] = 10; //Bats
-        mySpawns[1, 1] = 0; //Charger
-
-        //Round 3 
-        mySpawns[2, 0] = 8; //Bats
-        mySpawns[2, 1] = 1; //Charger
-
-        //Round 4
-        mySpawns[3, 0] = 13; //Bats
-        mySpawns[3, 1] = 2; //Charger
-
-        //Round 5
-        mySpawns[4, 0] = 20; //Bats
-        mySpawns[4, 1] = 4; //Charger
-
-        return mySpawns;
-    }
 }
